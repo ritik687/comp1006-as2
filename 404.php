0c241894da04729b98603e7489bca22e9511b083
ritik687 <200495974@student.georgianc.on.ca>
2022-04-08 19:59:22 -0400

    404 file is added and also error handling done

---
+++ 404.php
@@ -0,0 +1,12 @@
+<?php
+    $title ='We could not find the page';
+    require 'includes/header.php';
+?>
+
+
+<main class="container">
+      <h1>Hmm, that page Seems to be Missing....</h1>
+      <p> Please use one of the links above or try again</p>
+</main>
+</body>
+</html>
