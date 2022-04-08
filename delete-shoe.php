ea96afd22ee3f5d10d7373f164a18f904f5a74c6
ritik687 <200495974@student.georgianc.on.ca>
2022-04-08 17:53:28 -0400

    handled error on delete-shoe.php

--- delete-shoe.php
+++ delete-shoe.php
@@ -3,48 +3,55 @@
     $title='Deleting Shoe....';
     require 'includes/header.php';
 
+    try{
+            // get the selected shoeId from the url parameter using the $_GET array because get array will show you the shoeId in the URL
+            // $shoeId = $_GET['shoeId'];
 
-// get the selected shoeId from the url parameter using the $_GET array because get array will show you the shoeId in the URL
-// $shoeId = $_GET['shoeId'];
+            if(isset($_GET['shoeId']))
+            {
+                if (is_numeric($_GET['shoeId'])) 
+                {
 
-if(isset($_GET['shoeId']))
-{
-    if (is_numeric($_GET['shoeId'])) 
-    {
+                    $shoeId = $_GET['shoeId'];
 
-        $shoeId = $_GET['shoeId'];
+                    // connect
+                    // $db = new PDO('mysql:host=172.31.22.43;dbname=Ram200495974', 'Ram200495974', 'y4O4M_hDnR');
+                    // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // connect
-        // $db = new PDO('mysql:host=172.31.22.43;dbname=Ram200495974', 'Ram200495974', 'y4O4M_hDnR');
-        // $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                    require "includes/database.php";
 
-        require "includes/database.php";
+                    // running the sql delete command
+                    $sql = "DELETE FROM shoes WHERE shoeId = :shoeId"; // : the parameters
+                    $cmd = $db->prepare($sql);
+                    $cmd->bindParam(':shoeId', $shoeId, PDO::PARAM_INT);
+                    $cmd->execute();
 
-        // running the sql delete command
-        $sql = "DELETE FROM shoes WHERE shoeId = :shoeId"; // : the parameters
-        $cmd = $db->prepare($sql);
-        $cmd->bindParam(':shoeId', $shoeId, PDO::PARAM_INT);
-        $cmd->execute();
+                    // disconnect
+                    $db = null;
 
-        // disconnect
-        $db = null;
+                    //show message to the user
+                    echo '<h1>Shoe deleted</h1>
+                                <a href="shoes.php" class"alert alert-info">Back to Shoe List</a> ';
+                }
 
-        //show message to the user
-        echo '<h1>Shoe deleted</h1>
-                    <a href="shoes.php" class"alert alert-info">Back to Shoe List</a> ';
+                else{
+                    // we have a showId but its not a number
+                    echo 'Invalid Shoe'; 
+                }
+
+            }
+
+            else{
+                // shoeId is missing
+                echo 'Invalid Shoe';
+            }
     }
 
-    else{
-        // we have a showId but its not a number
-        echo 'Invalid Shoe'; 
-    }
-
-}
-
-else{
-    // shoeId is missing
-    echo 'Invalid Shoe';
-}
+    catch(Exception $error)
+        {
+            // an error happened so redirect to the error page 
+            header('location:error.php');
+        }
 
 // redirect again to the page shoes.php
 // header('location:shoes.php');
